f6f1e866f9923ede6180540803b85761eeda5823
mateusas3s <mateusaugusto-2009@hotmail.com>
2019-10-11 22:41:46 -0300

    refatoring functions

--- cavaleiros.pl
+++ cavaleiros.pl
@@ -12,7 +12,7 @@ cavaleiro(explosivo, virtuoso, neutro, capricornio).
 cavaleiro(moderado, humilde, leal, aquario).
 cavaleiro(moderado, arrogante, caotico, peixes).
 
-pergunta1 :-
+pergunta1(Resp1) :-
     write(
         'Como você se considera em relação ao seu temperamento?\n
         Digite o número da resposta:\n
@@ -20,16 +20,10 @@ pergunta1 :-
         2 - Moderado\n
         3 - Paciênte\n'
     ),
-    read(X),
-    switch_q1(X).
-        switch_q1(1) :-
-            Resp1 = explosivo.
-        switch_q1(2) :-
-            Resp1 = moderado.
-        switch_q1(3) :-
-            Resp1 = paciente.
+    read(Op1),
+    switch_q1(Op1, Resp1).
 
-pergunta2 :-
+pergunta2(Resp2) :-
     write(
         'Como você se considera em relação à sua personalidade?\n
         Digite o número da resposta:\n
@@ -37,16 +31,10 @@ pergunta2 :-
         2 - Virtuoso\n
         3 - Humilde\n'
     ),
-    read(Y),
-    switch_q2(Y).
-        switch_q2(1) :-
-            Resp2 = arrogante.
-        switch_q2(2) :-
-            Resp2 = virtuoso.
-        switch_q2(3) :-
-            Resp2 = humilde.
+    read(Op2),
+    switch_q2(Op2, Resp2).
 
-pergunta3 :-
+pergunta3(Resp3) :-
     write(
         'Como você se considera em relação ao seu senso de justiça?\n
         Digite o número da resposta:\n
@@ -54,25 +42,39 @@ pergunta3 :-
         2 - Neutro\n
         3 - Caótico\n'
     ),
-    read(Z),
-    switch_q3(Z).
-        switch_q3(1) :-
-            Resp3 = leal.
-        switch_q3(2) :-
-            Resp3 = neutro.
-        switch_q3(3) :-
-            Resp3 = caotico.
+    read(Op3),
+    switch_q3(Op3, Resp3).
+
+switch_q1(1, Resp1) :-
+    Resp1 = explosivo.
+switch_q1(2, Resp1) :-
+    Resp1 = moderado.
+switch_q1(3, Resp1) :-
+    Resp1 = paciente.
+
+switch_q2(1, Resp2) :-
+    Resp2 = arrogante.
+switch_q2(2, Resp2) :-
+    Resp2 = virtuoso.
+switch_q2(3, Resp2) :-
+    Resp2 = humilde.
+
+switch_q3(1, Resp3) :-
+    Resp3 = leal.
+switch_q3(2, Resp3) :-
+    Resp3 = neutro.
+switch_q3(3, Resp3) :-
+    Resp3 = caotico.
 
 seuCavaleiro(Resp1, Resp2, Resp3) :-
-    cavaleiro(Resp1, Resp2, Resp3, W),
-    write(Resp1) ,write(Resp2) ,write(Resp3), 
+    cavaleiro(Resp1, Resp2, Resp3, Cav),
     write('Você é o cavaleiro de '),
-    write(W).
+    write(Cav).
 
 start :-
-    pergunta1,
-    pergunta2,
-    pergunta3,
+    pergunta1(Resp1),
+    pergunta2(Resp2),
+    pergunta3(Resp3),
     seuCavaleiro(Resp1, Resp2, Resp3).
 
 
